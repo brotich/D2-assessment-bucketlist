0ff8b05825b382bb5789ee211b9e62fa08faf3d2
Brian Rotich <brian.rotich@andela.com>
2018-02-05 12:59:07 +0300

    add sample endpoint with response

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 import config
-from flask import Flask
 
 app = Flask(__name__)
 
 app.config.from_object(config)
 db = SQLAlchemy(app)
+
+from app.api.sample import sample_mod
+
+app.register_blueprint(sample_mod)
